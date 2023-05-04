f7aa77479d192461fdb2b02f968a60a7b1413c77
Aleksei Laptev <Hellwind@mail.ru>
2023-05-04 14:29:12 +0300

    fix: update script with update fix

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,7 +5,9 @@ const sourcePath = path.join(__dirname,"files");
 const destinationPath = path.join(__dirname,"files-copy");
 
 async function copyDir() {
+    await fs.rm(destinationPath,{recursive: true})
     await fs.mkdir(destinationPath,{recursive: true})
+    
     const files = await fs.readdir(sourcePath,{withFileTypes: true})
     files.forEach(async file => {
         await fs.copyFile(path.join(sourcePath,file.name), path.join(destinationPath,file.name));
